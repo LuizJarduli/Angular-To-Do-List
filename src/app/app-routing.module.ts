17011c5e58f8c7b031033a75a7880e43c4b21ade
LuizMiguel <luiz.jarduli@helpper.com.br>
2022-08-28 11:22:09 -0300

    feat: start project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
+import { HomeComponent } from './modules/home/pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  	{ path: '', component: HomeComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  	imports: [RouterModule.forRoot(routes)],
+  	exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
