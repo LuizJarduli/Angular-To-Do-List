65465ccc3a6f085bf7dd25b4afe3b62f833cd2e9
LuizMiguel <luiz.jarduli@helpper.com.br>
2022-08-28 15:26:31 -0300

    feat: handling two-way data binding on list

--- src/app/modules/home/components/to-do-list/to-do-list.component.ts
+++ src/app/modules/home/components/to-do-list/to-do-list.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { TaskList } from './../../model/task-list';
+import { Component, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.component.html',
   styleUrls: ['./to-do-list.component.scss']
 })
-export class ToDoListComponent implements OnInit {
+export class ToDoListComponent implements DoCheck {
 
-  constructor() { }
+    public taskList: Array<TaskList> = [
+        { name: 'Primeira tarefa', checked: true },
+        { name: 'Segunda tarefa', checked: false },
+    ];
 
-  ngOnInit(): void {
-  }
+    constructor() { }
 
+    ngDoCheck(): void {
+        this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+    }
+
+    public deleteItemTaskList(event: number): void {
+        this.taskList.splice(event, 1);
+    }
+
+    public deleteAllTaskList(): void {
+        const confirm: boolean = window.confirm('Você deseja realmente deletar tudo?');
+        confirm && (this.taskList = []);
+    }
+
+    public validationInput(event: any): void {
+        if (!event.currentText?.length) {
+            const confirm: boolean = window.confirm('Task está vazia, deseja deletar?');
+            confirm && this.deleteItemTaskList(event?.index);
+        }
+    }
+
+    public setEmmitTaskList(event: TaskList): void {
+        this.taskList.push(event);
+    }
 }
